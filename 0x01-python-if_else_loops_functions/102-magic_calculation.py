580b6faf837762e08615630182f60afae960a4f4
Trevorvaizel <trevor.vaizelstudio@gmail.com>
2023-06-13 18:37:55 +0000

    j

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,7 +1,9 @@
-#!/usr/bin/python3
-def magin_calculation(a, b, c):
+i#!/usr/bin/python3
+
+def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
     if a < b:
-        return c
-    elif c > b:
-        return a + b
-    return a * b - c
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
