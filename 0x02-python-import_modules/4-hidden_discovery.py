93de408ac3d6299b479eee06431192ca8c7e3673
Trevorvaizel <trevor.vaizelstudio@gmail.com>
2023-06-14 16:55:16 +0000

    4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+import hidden_4
+
+def dir_print():
+    definitions = dir(hidden_4)
+    definitions.sort()
+    for n in definitions:
+        if not n.startswith('_'):
+            print(n)
+
+dir_print()
