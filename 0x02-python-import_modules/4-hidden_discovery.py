db3ffb56443b6e747c56878573df39dde7ee92c8
Trevorvaizel <trevor.vaizelstudio@gmail.com>
2023-06-14 20:13:27 +0300

    4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import hidden_4
+# 4-hidden_discovery.py
 
-def dir_print():
-    definitions = dir(hidden_4)
-    definitions.sort()
-    for n in definitions:
-        if not n.startswith('_'):
-            print(n)
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
 
-dir_print()
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
